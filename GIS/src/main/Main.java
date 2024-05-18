3cea43aee23fa3971f642ae7bae1468f222394ed
cjain316 <114009355+cjain316@users.noreply.github.com>
2024-05-18 11:00:23 -0700

    excess imports

--- GIS/src/main/Main.java
+++ GIS/src/main/Main.java
@@ -1,7 +1,5 @@
 package main;
 
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -10,7 +8,6 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
-import java.awt.geom.AffineTransform;
 import java.net.URL;
 
 import javax.swing.JFrame;

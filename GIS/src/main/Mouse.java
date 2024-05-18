42f94dcf189d8ed1119139b8294a00a195132673
cjain316 <114009355+cjain316@users.noreply.github.com>
2024-05-18 11:49:40 -0700

    mouse stuff

---
+++ GIS/src/main/Mouse.java
@@ -0,0 +1,23 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Point;
+
+public class Mouse {
+    public int x,y;
+
+    public Mouse() {
+
+    }
+
+    public void setPosition(int x,int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(x,y,10,10);
+    }
+}
--- GIS/src/main/SysInf.java
+++ GIS/src/main/SysInf.java
@@ -5,6 +5,7 @@ import java.awt.*;
 public class SysInf {
     private Dimension screenBounds;
 
+
     public SysInf() {
         screenBounds = Toolkit.getDefaultToolkit().getScreenSize();
 

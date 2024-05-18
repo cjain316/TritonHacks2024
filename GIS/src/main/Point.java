8f5da29185570cc9f490f53c0e6dbeaba97f349a
DanTheMan6492 <90531006+DanTheMan6492@users.noreply.github.com>
2024-05-18 16:26:00 -0700

    Added ClosedLoop

--- GIS/src/main/Point.java
+++ GIS/src/main/Point.java
@@ -17,4 +17,24 @@ public class Point {
 	public void setPoint(Point p) {
 		nextPoint = p;
 	}
+
+	public static int dist(Point p1, Point p2){
+		return (int)Math.pow(p1.x - p2.x, 2) +
+				(int)Math.pow(p1.y - p2.y, 2);
+	}
+
+	// 0 --> Collinear
+	// 1 --> Clockwise
+	// 2 --> Counterclockwise
+	public static int orientation(Point p, Point q, Point r){
+		int val = (q.y - p.y) * (r.x - q.x) -
+				(q.x - p.x) * (r.y - q.y);
+		if (val == 0) return 0;  // collinear
+		return (val > 0)? 1: 2; // clockwise or counterclock wise
+	}
+
+	public String toString(){
+		return "(" + x + "," + y + ")";
+	}
+
 }

7849778c0ff3b1e98137b8fea01fb285c45921bc
cjain316 <114009355+cjain316@users.noreply.github.com>
2024-05-18 10:59:35 -0700

    #1 java paint setup

---
+++ GIS/src/main/Main.java
@@ -0,0 +1,97 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+public class Main extends JPanel implements KeyListener, ActionListener {
+    Timer c = new Timer(1,this);
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Main f = new Main();
+
+    }
+
+    public void paint(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        // TODO Auto-generated method stub
+        repaint();
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+        System.out.println(arg0.getExtendedKeyCode());
+    }
+
+    @Override
+    public void keyTyped(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    Timer t;
+
+    public Main() {
+        JFrame f = new JFrame("GIS");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(1920,1080);
+        f.add(this);
+        f.addKeyListener(this);
+        f.setResizable(false);
+
+        t = new Timer(60, this);
+        t.start();
+        f.setVisible(true);
+
+
+
+    }
+
+    private boolean randomBool() {
+        int ran = (int)(Math.random()*2);
+        if (ran == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected Image getImage(String path) {
+
+        Image tempImage = null;
+        try {
+            URL imageURL = Main.class.getResource(path);
+            tempImage    = Toolkit.getDefaultToolkit().getImage(imageURL);
+        } catch (Exception e) {e.printStackTrace();}
+        return tempImage;
+    }
+
+}
